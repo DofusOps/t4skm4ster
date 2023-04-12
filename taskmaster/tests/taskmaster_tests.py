12545c06d5f66a996ece758c1c50dc87596b72c1
jurichar <jurichar@student.42.fr>
2023-04-12 14:12:15 +0200

    add tests

--- taskmaster/tests/taskmaster_tests.py
+++ taskmaster/tests/taskmaster_tests.py
@@ -1,14 +1,12 @@
 import pytest
 import pexpect
 
-@pytest.mark.timeout(3)
 def test_reload():
     taskmaster = pexpect.spawn("python main.py")
     taskmaster.sendline("reload")
     taskmaster.expect("Config file loaded successfully")
     taskmaster.sendline("exit")
 
-@pytest.mark.timeout(3)
 def test_reload2():
     taskmaster = pexpect.spawn("python main.py")
     taskmaster.sendline("reload")

5c2b98f4bdd6afe133f6f9c17072ffef5fd2bb1d
César Claude <cesarclaudepro@gmail.com>
2023-04-13 17:21:21 +0200

    :construction_worker: refixing os.environ dict issue

--- taskmaster/tests/taskmaster_tests.py
+++ taskmaster/tests/taskmaster_tests.py
@@ -3,7 +3,7 @@ import os
 
 
 def test_reload():
-    taskmaster = pexpect.spawn("coverage run main.py", env=dict(os.environ) | {'COVERAGE_PROCESS_START': '.coveragerc'})
+    taskmaster = pexpect.spawn("coverage run main.py", env=dict(os.environ, COVERAGE_PROCESS_START='.coveragerc'))
     taskmaster.sendline("reload")
     taskmaster.expect("Config file loaded successfully")
     taskmaster.sendline("exit")
@@ -11,7 +11,7 @@ def test_reload():
 
 
 def test_help():
-    taskmaster = pexpect.spawn("coverage run main.py", env=dict(os.environ) | {'COVERAGE_PROCESS_START': '.coveragerc'})
+    taskmaster = pexpect.spawn("coverage run main.py", env=dict(os.environ, COVERAGE_PROCESS_START='.coveragerc'))
     taskmaster.sendline("help")
     taskmaster.expect("Available commands: status, start <program>, stop <program>, restart <program>, reload, exit")
     taskmaster.sendline("exit")
@@ -19,7 +19,7 @@ def test_help():
 
 
 def test_invalid():
-    taskmaster = pexpect.spawn("coverage run main.py", env=dict(os.environ) | {'COVERAGE_PROCESS_START': '.coveragerc'})
+    taskmaster = pexpect.spawn("coverage run main.py", env=dict(os.environ, COVERAGE_PROCESS_START='.coveragerc'))
     taskmaster.sendline("xxxxx")
     taskmaster.expect("Unknown command:xxxxx.")
     taskmaster.expect("Type 'help' for a list of available commands.")
